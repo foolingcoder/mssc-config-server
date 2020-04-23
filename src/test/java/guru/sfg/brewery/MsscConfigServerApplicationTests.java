8bea2faef940f0e1282f82f94b06e3af0732414f
angump <angump@host.docker.internal>
2020-04-23 13:05:08 +0400

    Corrected issue with failing test

--- src/test/java/guru/sfg/brewery/MsscConfigServerApplicationTests.java
+++ src/test/java/guru/sfg/brewery/MsscConfigServerApplicationTests.java
@@ -1,9 +1,7 @@
 package guru.sfg.brewery;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class MsscConfigServerApplicationTests {
 
 	@Test
